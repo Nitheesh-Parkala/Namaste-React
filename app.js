d821605b91b590a9b373dfdfa0ed62741736e392
Nitheesh26 <nitheeshprabhu2001@gmail.com>
2024-01-21 11:49:16 +0530

    completed episode 3

--- app.js
+++ app.js
@@ -4,16 +4,72 @@ import ReactDOM from "react-dom/client"
 
 
 const Header = ()=>{
-     <div className="logo-container">
-      <img src="2wCEAAoGBw4ODhAODg4ODg8ODg4ODg4ODhAPDg4PFhIYGBYSFhQaHjgiGhw0HRYWMDQlNC07PjIxGiFDPTcwOy0wMS4BCwsLDw4PHRERHTAoIigwMDkwMDYyMDIwLi4wMDAwMDIyMDIyMDAyMDAwMDIwMjAwMDEwMDIuMjAwMTAwMC4wMP"/>
-     </div>
-     
+     return (
+       <div className="header">
+         <div className="logo-container">
+           <img
+             className="logo"
+             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLTeCjHOeaBtOLYRJ0LdiG2-B49dfnEULULA&usqp=CAU"
+           />
+         </div>
+         <div className="nav-items">
+           <ul>
+             <li>Home</li>
+             <li>About US</li>
+             <li>Contact Us</li>
+             <li>Cart</li>
+           </ul>
+         </div>
+       </div>
+     );
 }
+const RestaurantCard =()=>{
+     return (
+       <div
+         className="res-card"
+         style={{
+           backgroundColor: "#f0f0f0",
+         }}
+       >
+         <img
+           className="res-img"
+           src="https://cdn.britannica.com/36/123536-050-95CB0C6E/Variety-fruits-vegetables.jpg"
+         />
+         <h3>Carzy Foods</h3>
+         <h4>Dry fruits</h4>
+         <h4>4.5 star</h4>
+         <h4>26 minutes</h4>
+       </div>
+     );
+}
+const Body = ()=>{
+     return (
+       <div className="body">
+         <div className="searchBar">SerachBar</div>
+         <div className="res-container">
+           {/* Restru component we have to do separte. */}
+           <RestaurantCard />
+           <RestaurantCard />
+           <RestaurantCard />
+           <RestaurantCard />
+           <RestaurantCard />
+           <RestaurantCard />
+           <RestaurantCard />
+           <RestaurantCard />
+         </div>
+       </div>
+     );
+}
+
 
 const AppLayOut = ()=>{
-
+    return (
+      <div className="app">
+        <Header />
+        <Body/>
+      </div>
+    );
 }
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayOut />);
